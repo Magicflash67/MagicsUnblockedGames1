bd0a7d9aab62fa228608a4e3d32683cf3ffd417e
infinity_Egg <132611485+Magicflash67@users.noreply.github.com>
2024-04-07 21:08:40 -0500

    having wifi issues and remote isussues. worked on the idel section. starting webtest/fixing. added a python script to faster make tabels

---
+++ extra/CPGen.py
@@ -0,0 +1,10 @@
+while True:
+ name = input("name")
+ dec = input("description")
+ creator = input("creator")
+ host = input("game host")
+
+ print("<td>" + name + "</td>")
+ print("<td>" + dec + "</td>")
+ print("<td>" + creator + "</td>")
+ print("<td>" + host + "</td>")
